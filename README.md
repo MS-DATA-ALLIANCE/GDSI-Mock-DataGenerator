153be103a3b48e79f264661442028b1f0bca975d
ashkan-pirmani <ashkan_pirmani@outlook.com>
2023-09-14 11:22:40 +0200

    additional script added

--- README.md
+++ README.md
@@ -7,11 +7,33 @@ This repository contains a Python script to generate mock data that resembles th
 
 The original dataset can be downloaded from [PhysioNet](https://physionet.org/content/patient-level-data-covid-ms/1.0.0/). It contains various features related to COVID-19 cases among multiple sclerosis (MS) patients.
 
-## How to Use the Mock Data Generator
+## How to Use the Mock Data Generator without tunable wights
 
 1. Download the original dataset from PhysioNet and place it in the same directory as the Python script (`mock_data_generator_updated.py`).
 2. Run the Python script. It will prompt you to enter the number of records you'd like to generate.
 3. The script will generate a CSV file (`generated_mock_data.csv`) containing the synthetic data.
+
+
+## How to Use the Mock Data Generator with tunable weights
+
+1. Download the original dataset from PhysioNet and place it in the same directory as the Python script (`mock_data_generator_with_weights.py`).
+2. Run the Python script. It will prompt you to enter the number of records you'd like to generate.
+3. The script will generate a CSV file (`generated_mock_data.csv`) containing the synthetic data.
+
+## How to Edit the Weights
+
+The script includes a dictionary called `weights_dict` that allows you to set custom weights for the occurrence of specific variables. Here's how it looks:
+
+```python
+weights_dict = {
+    "sex": {"female": 0.7, "male": 0.3},
+    "covid19_admission_hospital": {"no": 0.8, "yes": 0.2},
+    # ... other variables ...
+}
+```
+
+To change the weights, simply edit the corresponding values in the dictionary.
+
 
 ## How the Script Works
 
---
+++ mock_data_generator_with_weights.py
@@ -0,0 +1,83 @@
+
+import pandas as pd
+import random
+import numpy as np
+
+def generate_mock_data(num_records, unique_values_dict):
+    """
+    Generate mock data based on the unique values and data types found in the original dataset.
+    
+    Parameters:
+    - num_records: The number of records to generate.
+    - unique_values_dict: Dictionary containing unique values for each column in the original dataset.
+    
+    Returns:
+    - mock_df: A DataFrame containing the generated mock data.
+    """
+    
+    # Initialize an empty dictionary to store the generated data
+    mock_data = {}
+    
+    # Generate unique "secret_name" identifiers
+    mock_data["secret_name"] = [f"C_{1000 + i}" for i in range(num_records)]
+    
+    # Weights for specific columns
+    weights_dict = {
+        "sex": {"female": 0.7, "male": 0.3},
+        "covid19_admission_hospital": {"no": 0.8, "yes": 0.2},
+        "covid19_confirmed_case": {"yes": 0.6, "no": 0.4},
+        "has_comorbidities": {"yes": 0.5, "no": 0.5},
+        "current_dmt": {"yes": 0.4, "never_treated": 0.3, "no": 0.3},
+        "ms_type2": {"relapsing_remitting": 0.6, "progressive_MS": 0.3, "other": 0.1}
+    }
+    
+    # Generate data for each column except "secret_name"
+    for column, unique_values in unique_values_dict.items():
+        if column == "secret_name":
+            continue
+        if column in weights_dict:
+            # If the column has specified weights, use them
+            weights = [weights_dict[column].get(val, 0) for val in unique_values]
+            mock_data[column] = random.choices(unique_values, weights=weights, k=num_records)
+        elif df[column].dtype == 'object':
+            mock_data[column] = random.choices(unique_values, k=num_records)
+        elif df[column].dtype == 'int64':
+            mock_data[column] = random.choices(unique_values, k=num_records)
+        elif df[column].dtype == 'float64':
+            min_val = np.min(unique_values)
+            max_val = np.max(unique_values)
+            mock_data[column] = np.random.uniform(min_val, max_val, num_records)
+        else:
+            mock_data[column] = [np.nan] * num_records
+    
+    # Create a DataFrame from the generated data
+    mock_df = pd.DataFrame(mock_data)
+    
+    return mock_df
+
+# Load the original dataset
+df = pd.read_csv('GDSI_OpenDataset_Final.csv')
+
+# Prepare the unique values dictionary based on the original dataset
+unique_values_dict = {}
+for column in df.columns:
+    unique_values = df[column].dropna().unique()
+    unique_values_dict[column] = unique_values
+
+# Ask the user for the number of records to generate
+try:
+    num_records = int(input("How many records would you like to generate? "))
+    if num_records < 1:
+        print("The number of records should be at least 1.")
+    else:
+        # Generate the mock data
+        mock_df = generate_mock_data(num_records, unique_values_dict)
+        
+        # Save the mock data to a CSV file
+        mock_data_file_path = 'generated_mock_data.csv'
+        mock_df.to_csv(mock_data_file_path, index=False)
+        
+        print(f"Mock data has been generated and saved to {mock_data_file_path}.")
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
+
